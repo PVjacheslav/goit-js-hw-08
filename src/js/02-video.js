7eb00d8f667a1d7c2b858c52eb2a4890790cdde4
PVjacheslav <128468517+PVjacheslav@users.noreply.github.com>
2023-08-12 12:03:34 +0300

    solving second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+// const player = new Player('handstick', {
+//     id: "vimeo-player",
+//     width: 640
+// });
+
+const throttle = require('lodash.throttle');
+
+// player.on('play', function() {
+//     console.log('played the video!');
+// });
+
+let time;
+const timePlay = function (data) {
+    time = data.seconds;
+    // console.log(time)
+    localStorage.setItem('videoplayer-current-time', time);
+};
+
+player.setCurrentTime()
+player.on('timeupdate', throttle(timePlay, 1000));
