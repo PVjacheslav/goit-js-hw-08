11139e353e1f981d551316bb5fc7962dd471be75
PVjacheslav <128468517+PVjacheslav@users.noreply.github.com>
2023-08-12 17:26:57 +0300

    fixing second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,23 +1,32 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-// const player = new Player('handstick', {
-//     id: "vimeo-player",
-//     width: 640
-// });
+// // const player = new Player('handstick', {
+// //     id: "vimeo-player",
+// //     width: 640
+// // });
 
-const throttle = require('lodash.throttle');
+// const throttle = require('lodash.throttle');
 
-// player.on('play', function() {
-//     console.log('played the video!');
-// });
+// // player.on('play', function() {
+// //     console.log('played the video!');
+// // });
 
-let time;
-const timePlay = function (data) {
-    time = data.seconds;
-    // console.log(time)
-    localStorage.setItem('videoplayer-current-time', time);
-};
+// let time;
+// const timePlay = function (data) {
+//     time = data.seconds;
+//     // console.log(time)
+//     localStorage.setItem('videoplayer-current-time', time);
+// };
 
-player.setCurrentTime()
+// player.setCurrentTime()
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+function onPlay(data) { localStorage.setItem('videoplayer-current-time', data.seconds); 'videoplayer-current-time'};
+// let time;
+// const timePlay = function (data) {
+//     time = data.seconds;
+// };
 player.on('timeupdate', throttle(timePlay, 1000));
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'))
