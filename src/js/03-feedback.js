b70b9021d046de080b0ec2bf90968cd2a8a906a1
PVjacheslav <128468517+PVjacheslav@users.noreply.github.com>
2023-08-13 14:52:14 +0300

    solution of the third task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+import throttle from "lodash.throttle";
+import '../css/common.css';
+import '../css/03-feedback.css';
 
+const STORAGE_KEY = "feedback-form-state";
+const feedback = {
+    form: document.querySelector('.feedback-form'),
+    input: document.querySelector('.input'),
+    textarea: document.querySelector('.textarea'),
+}
+newFormOutput();
+
+const formData = {};
+feedback.form.addEventListener('input', throttle(hendlerFormOut, 500));
+function hendlerFormOut (evt) {
+    formData[evt.target.name] = evt.target.valua;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData))
+}
+
+feedback.form.addEventListener('submit', (evt) =>{
+    evt.preventDefault();
+
+    localStorage.removeItem(STORAGE_KEY);
+    evt.currentTarget.reset();
+});
+
+function newFormOutput() {
+    const savedMessage =JSON.parse(localStorage.getItem(STORAGE_KEY));
+    if(savedMessage) {
+        feedback.input.valua = savedMessage.email;
+        feedback.textarea.valua = savedMessage.message;
+    }
+}
